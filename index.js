01b4f36672b19d001fd5ea95796ef0cb3b99e3f6
Bhupinder <bhupindersingh987686@gmail.com>
2021-02-03 11:28:33 +0530

    commit

--- index.js
+++ index.js
@@ -314,34 +314,35 @@
 
 
 
-const express = require("express");
-const app = express();
-app.get("/", (req, res) => {
-    res.status(200).send("get request");
+// const express = require("express");
+// const app = express();
+// app.get("/", (req, res) => {
+//     res.status(200).send("get request");
+// })
+
+// let port = process.env.PORT || 5000;
+// app.listen(port,(err )=>
+// {
+//     if(err)
+//         console.log(err);
+//     else
+//         console.log(`Listening to port no ${port}`);
+// })  
+
+
+
+const fastify = require("fastify")();
+fastify.get('/', (req, rep) => {
+    rep.send("get request ok ok");
 })
 
 let port = process.env.PORT || 5000;
-app.listen(port,(err )=>
-{
+fastify.listen(port, (err) => {
     if(err)
-        console.log(err);
+        fastify.log.error(err);
     else
-        console.log(`Listening to port no ${port}`);
-})  
-
-
-
-// const fastify = require("fastify")();
-// fastify.get('/', (req, rep) => {
-//     rep.send({msg : "get request"});
-// })
-
-// fastify.listen(port, (err) => {
-//     if(err)
-//         fastify.log.error(err);
-//     else
-//     {
-//         console.log("hello");
-//         console.log(`Listening at port no ${port}`);
-//     }
-// })
+    {
+        console.log("hello");
+        console.log(`Listening at port no ${port}`);
+    }
+})
